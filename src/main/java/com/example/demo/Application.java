60c68d643fb9486d71ac0e7d118d173a29004f8e
max <singer0503@gmail.com>
2021-07-25 16:35:06 +0800

    first commit

---
+++ src/main/java/com/example/demo/Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("測試看看");
+		SpringApplication.run(Application.class, args);
+	}
+
+}
