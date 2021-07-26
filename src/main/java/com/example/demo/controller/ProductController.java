f3a48409b2b7e47e035533e8c6406bc629c9f218
max <singer0503@gmail.com>
2021-07-26 17:33:48 +0800

    類似 LinQ 的方法取資料, 有找到就回傳資料（狀態碼200），否則回傳狀態碼404。

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -8,18 +8,40 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @RestController     // @RestController 標記，代表這是一個 Controller
 @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)    // 藉由參數來定義回傳的資料格式為 JSON。
 public class ProductController {
 
+    // 以 Java 的 List 當作資料庫。
+    private final List<Product> productDB = new ArrayList<>();
+
+    @PostConstruct // javax 函式庫的 @PostConstruct 標記。這樣可以讓 Controller 被建立後，自動執行該方法
+    private void initDB(){
+        productDB.add(new Product("B0001", "Android Development (Java)", 380));
+        productDB.add(new Product("B0002", "Android Development (Kotlin)", 420));
+        productDB.add(new Product("B0003", "Data Structure (Java)", 250));
+        productDB.add(new Product("B0004", "Finance Management", 450));
+        productDB.add(new Product("B0005", "Human Resource Management", 330));
+    }
+
     @GetMapping("/products/{id}")       // @GetMapping 標記，再傳入資源路徑，即可配置一個 GET 請求 的 API
     public ResponseEntity<Product> getProduct(@PathVariable("id") String id){   //透過 @PathVariable 標記，獲取路徑中的 id 值。
-        Product product = new Product();
-        product.setId(id);
-        product.setName("Max");
-        product.setPrice(135);
 
-        // Spring Boot 提供了「回應實體」類別（ResponseEntity），讓開發者能將要回應的各種資訊，以一個物件來包裝
-        return ResponseEntity.ok().body(product);
+        // 類似 LinQ 的方法取資料  根據 id 在 productDB 這個 List 尋找產品
+        Optional<Product> productOp = productDB.stream().filter(p -> p.getId().equals(id)).findFirst();
+
+        // 有找到就回傳資料（狀態碼200），否則回傳狀態碼404。
+        if(productOp.isPresent()){
+            Product product = productOp.get();
+            return ResponseEntity.ok().body(product);
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+
     }
 }
--- src/main/java/com/example/demo/entity/Product.java
+++ src/main/java/com/example/demo/entity/Product.java
@@ -7,6 +7,11 @@ public class Product {
 
     public Product(){}
 
+    public Product(String id, String name, int price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
     public String getId() {
         return id;
     }
